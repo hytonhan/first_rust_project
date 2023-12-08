3cc7280b926ed955f93755c513a54f3c7e73fc26
Hannu Hytönen <hannu.hytonen@sarastia.fi>
2023-12-08 14:40:39 +0200

    day1 part1

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod day01;
+
+fn main() {
+    // println!("Hello, world!");
+
+    use std::io;
+
+    let mut input: String=String::new();
+    let mut exit: bool = false;
+    while exit == false
+    {
+        input = String::new();
+        println!("Enter day number: ");
+        match io::stdin().read_line(&mut input) {
+            Ok(_goes_into_input_above) => {},
+            Err(_no_updates_is_fine) => {},
+        }
+        input = input.trim().to_string();
+        println!("Input: {}", input);
+
+        let start_time: std::time::Instant = std::time::Instant::now();
+        match input.as_str() {
+            "1" => day01::day01(),
+            "-1" => exit = true,
+            _ => println!("wtf")
+        }
+        println!("Exec time: {:.2} ms", start_time.elapsed().as_millis());
+    }
+}
